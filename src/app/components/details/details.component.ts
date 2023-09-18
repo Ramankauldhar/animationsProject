65ea4031a652ed7914b78281d09c845d2542cd34
Raman kauldhar <rmnkaul979697@gmail.com>
2023-09-18 10:17:44 -0400

    Added New animations on details component

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-details',
@@ -6,5 +6,43 @@ import { Component} from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
- 
+  isAnimationActive = false; // Initialize as false
+
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    this.checkVisibility();
+  }
+
+
+  checkVisibility() {
+    const animation1 = document.querySelectorAll(".animationFromRight");
+    const animation2 = document.querySelectorAll(".animationFromLeft");
+    const windowHeight = window.innerHeight;
+    const elementVisible = 200;
+
+    // Check if at least one element has the 'active' class
+    this.isAnimationActive = Array.from(animation1).some((element) =>
+      element.classList.contains("active")
+    ) || Array.from(animation2).some((element) =>
+      element.classList.contains("active2")
+    );
+
+    animation1.forEach((element) => {
+      const elementTop = element.getBoundingClientRect().top;
+      if (elementTop < windowHeight - elementVisible) {
+        element.classList.add("active");
+      } else {
+        element.classList.remove("active");
+      }
+    });
+    animation2.forEach((element) => {
+      const elementTop = element.getBoundingClientRect().top;
+      if (elementTop < windowHeight - elementVisible) {
+        element.classList.add("active2");
+      } else {
+        element.classList.remove("active2");
+      }
+    });
+  }
 }
